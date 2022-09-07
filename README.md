a033cb04654c7f42e1dc5ec2ebfc2a85e4aa4426
willhooi <thisis@willhooi.com>
2022-09-07 22:27:57 +0800

    edited readme.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Notes to Future Self
 
 Day 0 > [Setup](notes/day0.md)
+
+Coding Practices >[Lessons from Kattis](notes/kattis_lessons.py)
---
+++ notes/kattis_lessons.py
@@ -0,0 +1,14 @@
+'''
+example of using map()
+-map() returns an object -> needs to be 'consumed' with list()
+-use test[1:] to only get items from index 1 onwards
+as discussed in IT5001
+'''
+
+def eoutlets():
+    N = int(input())
+    for i in range(N):
+        test=list(map(int,input().split()))
+        print((sum(test[1:])-test[0])+1)
+        
+eoutlets()
