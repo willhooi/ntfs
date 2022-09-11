15024a827f91874e2af0180247d8ee9ca4871ff5
willhooi <thisis@willhooi.com>
2022-09-11 23:38:25 +0800

    added is_anagram functions

--- notes/gccf1.py
+++ notes/gccf1.py
@@ -1,6 +1,8 @@
 '''
 Write a function burgerPrice(burger) to take in a string of customization and return a total sum for 
 the price
+
+Using dictionary/lookup table
 '''
 def burgerPrice(burger):
     total=[]
@@ -13,3 +15,34 @@ def burgerPrice(burger):
 print(burgerPrice('BVPB'))
 print(burgerPrice('BVCPCPB'))
 print(burgerPrice('BPVBPVCB'))
+
+'''
+Check anagram using dictionary by creating histogram/frquency count
+'''
+def is_anagram1(word1,word2): #verbose version
+    d1,d2={},{}
+    for i in word1.replace(' ',''):
+        if i not in d1: d1[i]=1
+        else: d1[i]+=1
+    for j in word2.replace(' ',''):
+        if j not in d2: d2[j]=1
+        else: d2[j]+=1
+    if d1==d2: return True
+    else: return False
+    
+def is_anagram2(word1,word2): #use .get so no need to use if
+    d1,d2={},{}
+    for char in word1.replace(' ',''):
+        d1[char]=d1.get(char,0)+1
+    for char in word2.replace(' ',''):
+        d2[char]=d2.get(char,0)+1
+    if d1==d2: return True
+    else: return False
+    
+def is_anagram3(word1,word2):
+    d1,d2={},{}
+    for char in word1.replace(' ',''):
+        d1[char]=d1.get(char,0)+1
+    for char in word2.replace(' ',''):
+        d2[char]=d2.get(char,0)+1
+    return d1==d2 # straightaway return the evaluated comparison, no need to use if
